896289d1236bc5108e0fa4c6696477ff61c0d1d8
Amadeus <nono02200059@gmail.com>
2020-03-30 21:47:15 +0800

    添加项目文件。

---
+++ EDCoder/Encoder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace EDCoder
+{
+    public static class Encoder
+    {
+        public static void GetBytes(Package pkg, ref byte[] bytes, ref int length)
+        {
+
+        }
+
+        private static int Compress(byte[] data, ref byte[] cdata, ref int clength)
+        {
+            using (MemoryStream msWrite = new MemoryStream(cdata))
+            {
+                //因为在写入的时候要压缩后写入，所以需要创建压缩流来写入(因此在压缩写入前需要先创建写入流)
+                //压缩的时候就是要将压缩好的数据写入到指定流中，通过fsWrite写入到新的路径下
+                using (GZipStream zip = new GZipStream(msWrite, CompressionLevel.Optimal, true))
+                {
+                    zip.Write(data, 0, data.Length);
+                }
+
+                // 每个 pkg 压缩后长度不得超过 int.MaxValue
+                if (msWrite.Position > int.MaxValue)
+                {
+                    throw new Exception("Too Much Data!: msWrite.Position > int.MaxValue");
+                }
+                else
+                {
+                    return (int)msWrite.Position;
+                }
+            }
+        }
+    }
+}
---
+++ RecordCore/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RecordCore
+{
+    public class Class1
+    {
+    }
+}
